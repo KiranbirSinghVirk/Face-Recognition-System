d244508122ade14784c37036af65cabb044eef8a
KiranbirSinghVirk <149593013+KiranbirSinghVirk@users.noreply.github.com>
2024-03-07 10:35:58 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-except sr.RequestError as e:
-    #     print("Could not request results from Google Speech Recognition service; {0}".format(e))
-    # finally:
-    #     speak_now_label.config(text="")  # Clear the message
